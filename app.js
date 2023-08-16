1fc4e6e0aaa910b817ad83d3dec81723ae5dddd9
Ad-gangwar <128047937+Ad-gangwar@users.noreply.github.com>
2023-08-16 16:32:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const request=require("request");
+const https=require("https");
+const app=express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname+"/signup.htm");
+});
+
+app.post("/", function(req, res){
+    const firstName=req.body.firstName;
+    const lastName=req.body.lastName;
+    const email=req.body.email;
+
+    const data={
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }  
+        ]
+    };
+    const jsonData=JSON.stringify(data);
+
+    const url="https://us21.api.mailchimp.com/3.0/lists/c0706b5fd7";
+
+    const options={
+        method: "POST",
+        auth: "adgangwar54:99118ea7fcfd09ce49e94694d489cd60-us21"
+    }
+    
+    const request=https.request(url, options, function(response){
+
+        if(response.statusCode === 200){
+            res.sendFile(__dirname+ "/success.htm")
+        } else{
+            res.sendFile(__dirname + "/failure.htm");
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+        
+    });
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.get("/failure", function(req, res){
+    res.redirect("/");
+});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("server is running at port 3000");
+});
+//list id c0706b5fd7
+//api key 99118ea7fcfd09ce49e94694d489cd60-us21
+
